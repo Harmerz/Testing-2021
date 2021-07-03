3bec7109f6f3f741f59df48876b38057feaf44a4
Farrel R <rafirizqy12@gmail.com>
2021-07-04 01:52:10 +0700

    Update to adapt to the wordpress theme layout

---
+++ src/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>Now you're seeing a React-based page!</p>
+        </div>
+    );
+}
