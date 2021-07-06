93a6cf8797cb24f68ea4ac4f9e1d463e0125968e
Farrel R <rafirizqy12@gmail.com>
2021-07-06 19:27:49 +0700

    temp: Revert auto-format to avoid merge conflict

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
-import App from './Main';
-import './index.css';
+import ReactDOM from "react-dom";
+import App from "./Main";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('content'));
+ReactDOM.render(<App />, document.getElementById("content"));
