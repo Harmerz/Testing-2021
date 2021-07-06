207e48c29119b5112e88fd54c86276227b434be7
Farrel R <rafirizqy12@gmail.com>
2021-07-07 02:12:59 +0700

    refactor: Remove over-abstractions on routing

--- src/pages/agenda/index.jsx
+++ src/pages/agenda/index.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
 export function Agenda() {
-  return (
-    <>
-      <h1>Agenda</h1>
-      <Link to="/">
-        Go to the home page (or any other pages; they share the same component
-        ATM) &rarr;
-      </Link>
-    </>
-  );
+    return (
+        <>
+            <h1>Agenda</h1>
+            <Link to='/'>
+                Go to the home page (or any other pages; they share the same
+                component ATM) &rarr;
+            </Link>
+        </>
+    );
 }
