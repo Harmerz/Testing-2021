386c1ec30b2d5851b14a5cd6d343d4c7058473bb
Farrel R <rafirizqy12@gmail.com>
2021-07-07 18:03:21 +0700

    Base refactor & Add coming soon page (#5)
    * feat: setup eslint + prettier
    
    * feat: vscode workspace settings.json: autoformat on save using eslint
    
    * feat: setup tailwindcss
    
    * temp: Revert auto-format to avoid merge conflict
    
    * refactor: (CANCELLED)Abstractize routes definition
    
    * revert: Remove typescript support
    
    * feat: Adjust workspace settings for tailwind
    
    * refactor: Rename global styles file
    
    * refactor: Some adjustments to the prettier config
    
    * refactor: Adjust eslint config
    
    * feat: Add coming soon page and its assets
    
    * refactor: Remove over-abstractions on routing
    
    * fix: Adjust some strings
    
    * fix: Modify deploy workflow to match the branches
    
    * feat: Dump existing field content before replacing
    
    * fix: Missing netlify credentials on draft deploy
    
    * fix: Rename some step titles
    
    * feat: Change page title to indicate coming-soon
    
    * fix: Further bury the testing page
    
    * fix: Import ordering
    
    * fix: Code formatting
    
    * fix: Try recreating the package-lock file
    
    * temp: Readd typescript-related files
    
    * fix: Properly remove ts configs on target branch
    
    * fix: Reattempt PR checks
    
    * style: Rename workflow title
    
    Co-authored-by: Lutfi Andriyanto <lutfiandri232@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,27 @@
-import ReactDOM from "react-dom";
-import App from "./Main";
+import { useEffect } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("content"));
+import './main.css';
+
+import * as pages from './pages/_listing';
+
+function Main() {
+    // FIXME
+    useEffect(() => {
+        document.title = 'Coming Soon — PPSMB Kesatria 2021';
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Switch>
+                {/* eslint-disable */}
+                <Route exact path='/agenda-testing' component={pages.Agenda} />
+                <Route                              component={pages.ComingSoon} />
+                {/* eslint-enable */}
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM.render(<Main />, document.getElementById('content'));
--- src/pages/DummyPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function DummyUnderConstructionsPage() {
-    return (
-        <>
-            <h1>PPSMB Kesatria 2021</h1>
-            <p>This site is currently under construction.</p>
-        </>
-    );
-}
