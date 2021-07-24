6d21a20d5f0b3c722454a1a84de54a46659001ec
Farrel R <rafirizqy12@gmail.com>
2021-07-24 22:58:29 +0700

    feat: Add simple hook for changing title

---
+++ src/utils/title.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+/**
+ * Changes the document title based on the currently-mounted page component
+ * @param {string} titleString 
+ */
+export function useTitle(titleString) {
+    return useEffect(() => {
+        document.title = titleString;
+    }, []);
+}
