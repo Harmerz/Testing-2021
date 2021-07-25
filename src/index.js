49e1a17c057e861c3b13cc10a834f1e27185c13d
Farrel R <rafirizqy12@gmail.com>
2021-07-25 22:45:50 +0700

    feat: Enforce strict mode in react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { Main } from './Main';
 
+const mainComponent = (
+    <StrictMode>
+        <Main />
+    </StrictMode>
+);
 const rootContainer = document.getElementById('content');
 
-render(<Main />, rootContainer);
+render(mainComponent, rootContainer);
