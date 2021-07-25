1781991881709615651681dfca34b3c93835ef43
Farrel R <rafirizqy12@gmail.com>
2021-07-25 16:48:12 +0700

    refactor: Provide our own custom brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,42 @@
 const forms = require('@tailwindcss/forms');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      screens: {
-        portrait: { raw: '(orientation: portrait)' },
-      },
-      colors: {
-        brand: {
-          dark: '#0C181D',
-          light: '#FFFFFF',
+    purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            screens: {
+                portrait: {
+                    raw: '(orientation: portrait)'
+                }
+            }
         },
-      },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: '#0D181D',
+            white: '#FFFFFF',
+            gray: {
+                lighter: '#F1F1F1',
+                darker: '#D9D8D3'
+            },
+            accent: {
+                yellow: '#F4A606',
+                red: '#DC3647',
+                blue: '#348AC5'
+            }
+        }
     },
-  },
-  variants: {
-    extend: {
-      dropShadow: ['hover'],
+    variants: {
+        extend: {
+            dropShadow: ['hover']
+        }
     },
-  },
-  plugins: [
-    forms({
-      strategy: 'class',
-    }),
-  ],
-  // corePlugins: {
-  //   preflight: false
-  // }
-  important: '#content'
+    plugins: [
+        forms({ strategy: 'class' })
+    ],
+    // corePlugins: {
+    //     preflight: false
+    // },
+    important: '#content'
 };
