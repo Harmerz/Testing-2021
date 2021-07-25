07f497c89ccce0944b80eb033813c4eb489949ae
Farrel R <rafirizqy12@gmail.com>
2021-07-26 00:15:16 +0700

    Add page header and footer (#16)
    * feat: Add simple hook for changing title
    
    * refactor: Separate back Main from app entry point
    
    * refactor: Provide our own custom brand colors
    
    * feat: Enforce strict mode in react
    
    * feat: Add brand font-face definitions (incomplete)
    
    * feat: Add dummy home page
    
    * feat: Done implementing page header
    
    Also, add a dummy footer for the moment
    
    * feat: Adjust main layout to stick footer to bottom
    
    * feat: (almost) done implementing footer
    
    * feat: Adjust action to deploy draft on any branch
    
    * fix: Code linting and styling

--- src/pages/webcomic/PresenceNavigationButton.jsx
+++ src/pages/webcomic/PresenceNavigationButton.jsx
@@ -4,7 +4,7 @@ export function PresenceNavigationButton({ progress, onClick }) {
         <button
             type='button'
             onClick={onClick}
-            className={`flex items-center absolute ${displayClass} right-20 bg-brand-light border-2 border-brand-dark px-6 py-2 rounded-lg transform duration-150 hover:-translate-y-1 hover:shadow-md`}
+            className={`flex items-center absolute ${displayClass} right-20 bg-white border-2 border-black px-6 py-2 rounded-lg transform duration-150 hover:-translate-y-1 hover:shadow-md`}
         >
             <div className='mr-2 text-sm lg:text-base'>Presensi</div>
             <svg
