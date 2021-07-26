2c41787b3007a33ee9a9f76941bd80e05c828c3f
Farrel R <rafirizqy12@gmail.com>
2021-07-26 22:32:52 +0700

    feat: Enable JIT support on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const forms = require('@tailwindcss/forms');
 
 module.exports = {
+    mode: 'jit',
     purge: ['./src/**/*.{js,jsx,ts,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
