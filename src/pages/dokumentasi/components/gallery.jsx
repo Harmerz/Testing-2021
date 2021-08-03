c1c22ade691f557c26097f2b8b0ea337fbd9ca20
nfhakim <60864368+nfhakim@users.noreply.github.com>
2021-08-03 20:39:22 +0700

    using local images
    -add new images

--- src/pages/dokumentasi/components/gallery.jsx
+++ src/pages/dokumentasi/components/gallery.jsx
@@ -1,4 +1,14 @@
 import React from 'react';
+import foto1 from '../../../assets/dokumentasi/1.jpg';
+import foto2 from '../../../assets/dokumentasi/2.jpg';
+import foto3 from '../../../assets/dokumentasi/3.jpg';
+import foto4 from '../../../assets/dokumentasi/4.jpg';
+import foto5 from '../../../assets/dokumentasi/5.jpg';
+import foto6 from '../../../assets/dokumentasi/6.jpg';
+import foto7 from '../../../assets/dokumentasi/7.jpg';
+import foto8 from '../../../assets/dokumentasi/8.jpg';
+import foto9 from '../../../assets/dokumentasi/9.jpg';
+
 
 export function Gallery() {
     function preview(source){
@@ -19,11 +29,15 @@ export function Gallery() {
     <div className='wrapper'>
         <nav>
             <div className='gallery'>
-                <div className='picture' data-name='2019' onClick={() => preview('https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_0183.jpg')} aria-hidden='true'><span><img src='https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_0183.jpg' alt='' /></span></div>
-                <div className='picture' data-name='2019' onClick={() => preview('https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_0120.jpg')} aria-hidden='true'><span><img src='https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_0120.jpg' alt='' /></span></div>
-                <div className='picture' data-name='2019' onClick={() => preview('https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/DSC_2408.jpg')} aria-hidden='true'><span><img src='https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/DSC_2408.jpg' alt='' /></span></div>
-                <div className='picture' data-name='2019' onClick={() => preview('https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_5688.jpg')} aria-hidden='true'><span><img src='https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_5688.jpg' alt='' /></span></div>
-                <div className='picture' data-name='2019' onClick={() => preview('https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_1275.jpg')} aria-hidden='true'><span><img src='https://ppsmb.ft.ugm.ac.id/wp-content/uploads/sites/1314/IMG_1275.jpg' alt='' /></span></div>
+                <div className='picture' data-name='2019' onClick={() => preview(foto1)} aria-hidden='true'><span><img src={foto1} alt='' /></span></div>
+                <div className='picture' data-name='2019' onClick={() => preview(foto2)} aria-hidden='true'><span><img src={foto2} alt='' /></span></div>
+                <div className='picture' data-name='2019' onClick={() => preview(foto3)} aria-hidden='true'><span><img src={foto3} alt='' /></span></div>
+                <div className='picture' data-name='2020' onClick={() => preview(foto7)} aria-hidden='true'><span><img src={foto7} alt='' /></span></div>
+                <div className='picture' data-name='2020' onClick={() => preview(foto8)} aria-hidden='true'><span><img src={foto8} alt='' /></span></div>
+                <div className='picture' data-name='2020' onClick={() => preview(foto9)} aria-hidden='true'><span><img src={foto9} alt='' /></span></div>
+                <div className='picture' data-name='2019' onClick={() => preview(foto4)} aria-hidden='true'><span><img src={foto4} alt='' /></span></div>
+                <div className='picture' data-name='2019' onClick={() => preview(foto5)} aria-hidden='true'><span><img src={foto5} alt='' /></span></div>
+                <div className='picture' data-name='2019' onClick={() => preview(foto6)} aria-hidden='true'><span><img src={foto6} alt='' /></span></div>
             </div>
         </nav>
         <nav>
@@ -33,7 +47,7 @@ export function Gallery() {
                 <span className='icon fas fa-times rounded-full' onClick={closeIcon} aria-hidden='true'>x</span>
             </div>
             <div className='image-box'>
-                <img src='' alt='' />
+                <img src={foto1} alt='' />
             </div>
             </div>
             <div className='shadow' />
