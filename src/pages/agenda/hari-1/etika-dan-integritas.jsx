3168c5300d2b564cf9818e3aa3e2f47d0c6d6357
Farrel R <rafirizqy12@gmail.com>
2021-08-03 08:29:12 +0700

    feat: Add WIP pages (still hidden from public view)

--- src/pages/agenda/hari-1/etika-dan-integritas.jsx
+++ src/pages/agenda/hari-1/etika-dan-integritas.jsx
@@ -1,18 +1,14 @@
-export const title = 'Jelajah Lembaga';
+export const title = '[WIP] Etika dan Integritas Akademik';
 
 export const content = (
     <div className='max-w-screen-sm mx-auto'>
         <iframe
-            src='https://www.youtube.com/embed/videoseries?list=PLx6TqpOpyeYPKkkQnkixPuV4w8aB-4h6z&rel=0'
+            src='https://www.youtube.com/embed/UtPKlHjsLTw'
             title='YouTube video player'
             frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
             allowFullScreen
             className='w-full h-96'
         />
-        <p className='text-center mt-4 font-body'>
-            Silakan lakukan navigasi antarvideo melalui tombol <em>Previous</em>{' '}
-            dan <em>Next</em> di YouTube yang tersedia.
-        </p>
     </div>
 );
---
+++ src/pages/agenda/hari-1/ppks.jsx
@@ -0,0 +1,12 @@
+export const title = <>[WIP] Kekerasan Seksual</>;
+
+export const content = (
+    <div className='max-w-screen-sm mx-auto'>
+        <iframe
+            src='https://drive.google.com/file/d/1FyCbvAtbQ1f7PHRvBD-8_90O3Kgu4nWH/preview'
+            allow='autoplay'
+            title='Slide tentang materi kekerasan seksual'
+            className='w-full h-96'
+        />
+    </div>
+);
