f112ef98888c993f10193f2eb2a432db9edc5df0
iskan.mustamir <iskan.mustamir@365.ugm.ac.id>
2021-08-04 07:30:21 +0700

    fix: gambar taman sd, halaman jalan-teknik

--- src/pages/jalan-teknik/index.jsx
+++ src/pages/jalan-teknik/index.jsx
@@ -1,38 +1,20 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Map } from './Map';
 import bg from '../../styles/Backdrop.module.css';
+import { GenericPage, PageTitle } from '../../components/GenericPage';
 
 export function JalanTeknik() {
     useEffect(() => {
         document.title = 'Jalan-Jalan Teknik';
     }, []);
     return (
-        <main
+        <GenericPage
             className={`${bg.backdrop} w-full bg-contain bg-no-repeat bg-bottom xl:bg-cover flex flex-col items-center justify-center font-display`}
         >
+            <PageTitle>Jalan-Jalan Teknik</PageTitle>
             <div className='w-5/6 md:w-3/4 flex flex-col items-center justify-start '>
-                <h1 className='text-2xl md:text-3xl font-bold pb-4'>
-                    JALAN-JALAN TEKNIK
-                </h1>
                 <Map />
-                <div className='flex sm:hidden items-center h-auto w-full justify-between pt-6'>
-                    <Link to='/jalan-teknik'>
-                        <img
-                            className='w-28'
-                            src='/assets/map/prev-button.svg'
-                            alt='button'
-                        />
-                    </Link>
-                    <Link to='/jalan-teknik'>
-                        <img
-                            className='w-28'
-                            src='/assets/map/next-button.svg'
-                            alt='button'
-                        />
-                    </Link>
-                </div>
             </div>
-        </main>
+        </GenericPage>
     );
 }
