cf7332527f4a610e8189ed0e9008081e80007a26
Farrel R <rafirizqy12@gmail.com>
2021-08-05 06:56:14 +0700

    page: Open talkshow page

---
+++ src/pages/agenda/hari-2/talkshow.jsx
@@ -0,0 +1,14 @@
+export const title = 'Talkshow';
+
+export const content = (
+    <div className='max-w-screen-sm mx-auto'>
+        <iframe
+            src='https://www.youtube.com/embed/RXp9npbe0_A?rel=0'
+            title='YouTube video player'
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+            className='w-full h-96'
+        />
+    </div>
+);
