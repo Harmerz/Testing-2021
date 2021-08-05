7d72ed6aa9d26a3ee9d7fb5f94aa0ec18718a7ad
Farrel R <rafirizqy12@gmail.com>
2021-08-05 08:03:37 +0700

    feat: Make Talkshow video auto-playing

--- src/pages/agenda/hari-2/talkshow.jsx
+++ src/pages/agenda/hari-2/talkshow.jsx
@@ -3,7 +3,7 @@ export const title = 'Talkshow';
 export const content = (
     <div className='max-w-screen-sm mx-auto'>
         <iframe
-            src='https://www.youtube.com/embed/RXp9npbe0_A?rel=0'
+            src='https://www.youtube.com/embed/RXp9npbe0_A?autoplay=1&rel=0'
             title='YouTube video player'
             frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
